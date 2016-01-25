4f21972ec88dc87ef21067a92076fea2ad02013b
zuli <zuli@linkedin.com>
2016-01-25 15:00:25 -0800

    Add 2 card pre-defined for dealer and logic for dealer and player to judge win and lose.

--- Blackjack/ViewController.swift
+++ Blackjack/ViewController.swift
@@ -39,7 +39,9 @@ class ViewController: UIViewController {
     var playerScore:Int = 0
     var cardIndex:Int = 0
     var cardMax:Int = 0
-    var cardCounter:Int = 1
+    var playerCardCounter:Int = 1
+    var dealerCardCounter:Int = 1
+    var dealerHiddenCard: UIImage = UIImage(named: "cardback")!
     
     // var cardNumberArray:[String] = ["card1", "card2", "card3", "card4", "card5", "card6", "card7", "card8", "card9", "card10", "card11", "card12", "card13"]
     
@@ -68,80 +70,194 @@ class ViewController: UIViewController {
             self.ForthCardView.image = UIImage(named: "")
             self.FifthCardView.image = UIImage(named: "")
             self.SixthCardView.image = UIImage(named: "")
-            self.dealerFirstCardView.image = UIImage(named: "")
-            self.dealerSecondCardView.image = UIImage(named: "")
+        }
+        if(self.dealerScore == 0) {
+            self.dealerCardImageView.image = UIImage(named: "cardback")
+            let dealerCardString:String = "card" + String(self.default_cards[self.cardIndex])
+            if(default_cards[self.cardIndex] >= 10) {
+                self.dealerScore += 10
+            }
+            else if(default_cards[self.cardIndex] == 1) {
+                if(self.dealerScore + default_cards[self.cardIndex] <= 21){
+                    self.dealerScore += 11
+                }
+                else {
+                    self.dealerScore += 1
+                }
+            }
+            else {
+                self.dealerScore += default_cards[self.cardIndex]
+            }
+            self.dealerFirstCardView.image = UIImage(named: dealerCardString)
+            self.cardIndex++
+            if(self.cardIndex == cardMax){
+                default_cards = default_cards.shuffle()
+                self.cardIndex = 0
+            }
+            
+            if(default_cards[self.cardIndex] >= 10) {
+                self.dealerScore += 10
+            }
+            else if(default_cards[self.cardIndex] == 1) {
+                if(self.dealerScore + default_cards[self.cardIndex] <= 21){
+                    self.dealerScore += 11
+                }
+                else {
+                    self.dealerScore += 1
+                }
+            }
+            else {
+                self.dealerScore += default_cards[self.cardIndex]
+            }
+            self.dealerSecondCardView.image = UIImage(named: "cardback")
+            self.dealerHiddenCard = UIImage(named: "card" + String(self.default_cards[self.cardIndex]))!
+            self.cardIndex++
+            if(self.cardIndex == cardMax){
+                default_cards = default_cards.shuffle()
+                self.cardIndex = 0
+            }
+            print(self.dealerScore)
+            self.dealerCardCounter = 3
             self.dealerThirdCardView.image = UIImage(named: "")
             self.dealerForthCardView.image = UIImage(named: "")
             self.dealerFifthCardView.image = UIImage(named: "")
             self.dealerSixthCardView.image = UIImage(named: "")
         }
 
-        let dealerCardString:String = "card" + String(self.default_cards[cardIndex])
-        self.dealerScore += default_cards[cardIndex]
-        self.dealerScoreLable.text = String(self.dealerScore)
-        cardIndex++
-        if(cardIndex == cardMax){
-            default_cards = default_cards.shuffle()
-            cardIndex = 0
+        let playerCardString:String = "card" + String(self.default_cards[self.cardIndex])
+        if(default_cards[self.cardIndex] >= 10) {
+            self.playerScore += 10
         }
-        self.dealerCardImageView.image = UIImage(named: dealerCardString)
-        
-        let playerCardString:String = "card" + String(self.default_cards[cardIndex])
-        self.playerScore += default_cards[cardIndex]
+        else if(default_cards[self.cardIndex] == 1) {
+            if(self.playerScore + default_cards[self.cardIndex] <= 21){
+                self.playerScore += 11
+            }
+            else {
+                self.playerScore += 1
+            }
+        }
+        else {
+            self.playerScore += default_cards[self.cardIndex]
+        }
         self.playerScoreLable.text = String(self.playerScore)
-        cardIndex++
-        if(cardIndex == cardMax){
+        self.cardIndex++
+        if(self.cardIndex == cardMax){
             default_cards = default_cards.shuffle()
-            cardIndex = 0
+            self.cardIndex = 0
         }
         self.playerCardImageView.image = UIImage(named: playerCardString)
         
         // Display cards
-        if (self.cardCounter == 1) {
+        if (self.playerCardCounter == 1) {
             self.FirstCardView.image = UIImage(named: playerCardString)
-            self.dealerFirstCardView.image = UIImage(named: dealerCardString)
         }
-        else if (self.cardCounter == 2) {
+        else if (self.playerCardCounter == 2) {
             self.SecondCardView.image = UIImage(named: playerCardString)
-            self.dealerSecondCardView.image = UIImage(named: dealerCardString)
         }
-        else if (self.cardCounter == 3) {
+        else if (self.playerCardCounter == 3) {
             self.ThirdCardView.image = UIImage(named: playerCardString)
-            self.dealerThirdCardView.image = UIImage(named: dealerCardString)
         }
-        else if (self.cardCounter == 4) {
+        else if (self.playerCardCounter == 4) {
             self.ForthCardView.image = UIImage(named: playerCardString)
-            self.dealerForthCardView.image = UIImage(named: dealerCardString)
         }
-        else if (self.cardCounter == 5) {
+        else if (self.playerCardCounter == 5) {
             self.FifthCardView.image = UIImage(named: playerCardString)
-            self.dealerFifthCardView.image = UIImage(named: dealerCardString)
         }
-        else if (self.cardCounter == 6) {
+        else if (self.playerCardCounter == 6) {
             self.SixthCardView.image = UIImage(named: playerCardString)
-            self.dealerSixthCardView.image = UIImage(named: dealerCardString)
         }
         else{
             // To be handled..
         }
-        self.cardCounter++
+        self.playerCardCounter++
         
         // Judge cards value
         if (self.playerScore == 21) {
             self.playerCardImageView.image = UIImage(named:"iTunesArtwork")
-            self.cardCounter = 1
+            self.playerCardCounter = 1
+            self.dealerCardCounter = 1
             self.playerScore = 0
             self.dealerScore = 0
         }
         else if (self.playerScore > 21) {
             self.playerCardImageView.image = UIImage(named:"frontlable")
-            self.cardCounter = 1
+            self.playerCardCounter = 1
+            self.dealerCardCounter = 1
             self.playerScore = 0
             self.dealerScore = 0
         }
     }
     
     @IBAction func StandTapped(sender: UIButton) {
+        self.dealerSecondCardView.image = self.dealerHiddenCard
+        self.dealerHiddenCard = UIImage(named: "cardback")!
+        
+        while(self.dealerScore <= self.playerScore) {
+            let dealerCardString:String = "card" + String(self.default_cards[self.cardIndex])
+            if(default_cards[self.cardIndex] >= 10) {
+                self.dealerScore += 10
+            }
+            else if(default_cards[self.cardIndex] == 1) {
+                if(self.dealerScore + default_cards[self.cardIndex] <= 21){
+                    self.dealerScore += 11
+                }
+                else {
+                    self.dealerScore += 1
+                }
+            }
+            else {
+                self.dealerScore += default_cards[self.cardIndex]
+            }
+            print(self.dealerScore)
+            print(dealerCardString)
+            self.dealerScoreLable.text = String(self.dealerScore)
+            self.dealerCardImageView.image = UIImage(named: dealerCardString)
+            
+            print(self.dealerCardCounter)
+            // Display cards
+            if (self.dealerCardCounter == 3) {
+                self.dealerThirdCardView.image = UIImage(named: dealerCardString)
+            }
+            else if (self.dealerCardCounter == 4) {
+                self.dealerForthCardView.image = UIImage(named: dealerCardString)
+            }
+            else if (self.dealerCardCounter == 5) {
+                self.dealerFifthCardView.image = UIImage(named: dealerCardString)
+            }
+            else if (self.dealerCardCounter == 6) {
+                self.dealerSixthCardView.image = UIImage(named: dealerCardString)
+            }
+            else{
+                // To be handled..
+            }
+            self.dealerCardCounter++
+            
+            self.cardIndex++
+            if(self.cardIndex == cardMax){
+                default_cards = default_cards.shuffle()
+                self.cardIndex = 0
+            }
+            
+            // Judge cards value
+            if (self.dealerScore <= 21) {
+                if(self.dealerScore == 21 || self.dealerScore > self.playerScore) {
+                    self.dealerCardImageView.image = UIImage(named:"iTunesArtwork")
+                    self.playerCardCounter = 1
+                    self.dealerCardCounter = 1
+                    self.playerScore = 0
+                    self.dealerScore = 0
+                    break;
+                }
+            }
+            else {
+                self.dealerCardImageView.image = UIImage(named:"frontlable")
+                self.playerCardCounter = 1
+                self.dealerCardCounter = 1
+                self.playerScore = 0
+                self.dealerScore = 0
+                break;
+            }
+        }
     }
     
     @IBAction func DoubleTapped(sender: UIButton) {
